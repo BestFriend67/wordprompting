9bd87e68e4f2996683ab881ecb811417ae2601b3
BestFriend67 <techworkmail67@gmail.com>
2024-05-14 16:37:47 +0800

    2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Choose a CSV file")
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
@@ -17,5 +17,5 @@ if uploaded_file is not None:
     # st.write(string_data)
 
     # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
+    dataframe = pd.ExcelFile(uploaded_file)
     st.write(dataframe)
