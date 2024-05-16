627b2f5301dc2b789fd030a0ad1a30d3fbb0709c
BestFriend67 <techworkmail67@gmail.com>
2024-05-17 03:39:13 +0800

    feat: requirements.txt is added

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,33 +24,33 @@ if uploaded_file is not None:
 
     print("file_name: ", file_name)
 
-    with open(file_name, 'w', newline='') as csvfile:
-        # creating a csv writer object
-        csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    # with open(file_name, 'w', newline='') as csvfile:
+    #     # creating a csv writer object
+    #     csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
 
-        for data in dataframe:
+    #     for data in dataframe:
 
-            if not pd.isna(data):
+    #         if not pd.isna(data):
                     
-                    print("data: ", data)
+    #                 print("data: ", data)
 
-                    response = client.chat.completions.create(
-                        model="gpt-4-0125-preview",
-                        messages=[
-                            {"role": "system", "content": SYSTEM_PROMPT},
-                            {"role": "user", "content": f"Provide definitions of {data}"}
-                        ]
-                    )
+    #                 response = client.chat.completions.create(
+    #                     model="gpt-4-0125-preview",
+    #                     messages=[
+    #                         {"role": "system", "content": SYSTEM_PROMPT},
+    #                         {"role": "user", "content": f"Provide definitions of {data}"}
+    #                     ]
+    #                 )
     
-                    result = response.choices[0].message.content
-                    results = result.split("\n")
-                    results.insert(0, data)
-                    # results = ",".join(results) + "\n"
+    #                 result = response.choices[0].message.content
+    #                 results = result.split("\n")
+    #                 results.insert(0, data)
+    #                 # results = ",".join(results) + "\n"
 
-                    csvwriter.writerow(results)
+    #                 csvwriter.writerow(results)
     
-    with open(file_name) as csvfile:                   
-        st.download_button("Download final results", csvfile, file_name=file_name)
+    # with open(file_name) as csvfile:                   
+    #     st.download_button("Download final results", csvfile, file_name=file_name)
         
 
     
